6fbd9e29ab65d53e05e512dd8e0179fa44c84073
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 16:51:05 +0300

    Fixed typo in folder name and in usings of AmdmService.cs

--- AMDM/AMDM.BLL/Infrastructure/ValidationException.cs
+++ AMDM/AMDM.BLL/Infrastructure/ValidationException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AMDM.BLL.Infastructure
+namespace AMDM.BLL.Infrastructure
 {
     class ValidationException : Exception
     {
--- AMDM/AMDM.BLL/Services/AmdmService.cs
+++ AMDM/AMDM.BLL/Services/AmdmService.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AMDM.BLL.Infastructure;
+using AMDM.BLL.Infrastructure;
 
 namespace AMDM.BLL.Services
 {
