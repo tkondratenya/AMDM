a60e50f94db3ebf23b59311c0c4fa3be29e200f2
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 16:02:14 +0300

    Updated PerformerDTO and created SongDTO

--- AMDM/AMDM.BLL/DTO/PerformerDTO.cs
+++ AMDM/AMDM.BLL/DTO/PerformerDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AMDM.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace AMDM.BLL.DTO
 {
-    class PerformerDTO
+    public class PerformerDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Song> Songs { get; set; }
         public string ImageLink { get; set; }
         public string PerformerPageLink { get; set; }
         public string FounderName { get; set; }
---
+++ AMDM/AMDM.BLL/DTO/SongDTO.cs
@@ -0,0 +1,20 @@
+﻿using AMDM.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMDM.BLL.DTO
+{
+    public class SongDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Performer Performer { get; set; }
+        public string Text { get; set; }
+        public ICollection<Chord> Chords { get; set; }
+        public int Views { get; set; }
+        public string VideoLink { get; set; }
+    }
+}
