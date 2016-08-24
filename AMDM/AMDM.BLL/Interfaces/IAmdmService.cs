75e9e9c786c3aba7c96e1708bf455d4c74ff08dd
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 16:10:45 +0300

    Added IAmdmService interface file to BLL

---
+++ AMDM/AMDM.BLL/Interfaces/IAmdmService.cs
@@ -0,0 +1,20 @@
+﻿using AMDM.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMDM.BLL.Interfaces
+{
+    public interface IAmdmService
+    {
+        IEnumerable<PerformerDTO> GetPerformers();
+        IEnumerable<SongDTO> GetSongs();
+        IEnumerable<ChordDTO> GetChord();
+        PerformerDTO GetPerformer(int? id);
+        SongDTO GetSong(int? id);
+        ChordDTO GetChord(int? id);
+        void Dispose();
+    }
+}
