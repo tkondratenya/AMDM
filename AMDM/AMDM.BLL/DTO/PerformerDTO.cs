3260715cb6b114fd2bba41e4fdb6a3eb27872efb
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 15:57:56 +0300

    Added link to DAL and created PerformerDTO

---
+++ AMDM/AMDM.BLL/DTO/PerformerDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMDM.BLL.DTO
+{
+    class PerformerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set; }
+        public string PerformerPageLink { get; set; }
+        public string FounderName { get; set; }
+        public string BiographyText { get; set; }
+    }
+}
