e8bbceac126aaaa78a1deaa6230006e3e880eea9
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 16:03:44 +0300

    Created ChordDTO

---
+++ AMDM/AMDM.BLL/DTO/ChordDTO.cs
@@ -0,0 +1,17 @@
+﻿using AMDM.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMDM.BLL.DTO
+{
+    public class ChordDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set; }
+        public ICollection<Song> Songs { get; set; }
+    }
+}
