8450e881b9d1a68a0af9820df324570d4159acad
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 17:23:52 +0300

    Added SongViewModel.cs to AMDM.WEB/Models

---
+++ AMDM/AMDM.WEB/Models/SongViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMDM.WEB.Models
+{
+    public class SongViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public PerformerViewModel Performer { get; set; }
+        public string Text { get; set; }
+        public ICollection<ChordViewModel> Chords { get; set; }
+        public int Views { get; set; }
+        public string VideoLink { get; set; }
+    }
+}
