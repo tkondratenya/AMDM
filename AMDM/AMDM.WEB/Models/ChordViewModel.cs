c3ed5a6ee27c6f7810d19660f58beb1b56b14de1
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 17:25:34 +0300

    Added ChordViewModel.cs to AMDM.WEB/Models

---
+++ AMDM/AMDM.WEB/Models/ChordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMDM.WEB.Models
+{
+    public class ChordViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set; }
+        public ICollection<SongViewModel> Songs { get; set; }
+    }
+}
