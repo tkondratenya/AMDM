1ed87da4055b548f7f6b871f30e992f3d319b98f
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 17:07:43 +0300

    Added ServiceModule.cs to AMDM.BLL/Infrastructure

---
+++ AMDM/AMDM.BLL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AMDM.DAL.Interfaces;
+using AMDM.DAL.Repositories;
+using Ninject.Modules;
+
+namespace AMDM.BLL.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
