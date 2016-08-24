afde0689ea6bae11301d50e624cf26aca778a7df
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 17:21:59 +0300

    Added PerformerViewModel.cs to AMDM.WEB/Models

---
+++ AMDM/AMDM.WEB/Models/PerformerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMDM.WEB.Models
+{
+    public class PerformerViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<SongViewModel> Songs { get; set; }
+        public string ImageLink { get; set; }
+        public string PerformerPageLink { get; set; }
+        public string FounderName { get; set; }
+        public string BiographyText { get; set; }
+    }
+}
