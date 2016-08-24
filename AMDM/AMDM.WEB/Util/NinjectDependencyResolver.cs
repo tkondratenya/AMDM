7d6831a587d13f88eeb65adde8cd208f57fd65b6
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 17:49:08 +0300

    Added NinjectDependencyResolver.cs to AMDM.WEB/Util

---
+++ AMDM/AMDM.WEB/Util/NinjectDependencyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AMDM.BLL.Interfaces;
+using Ninject;
+using AMDM.BLL.Services;
+
+namespace AMDM.WEB.Util
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IAmdmService>().To<AmdmService>();
+        }
+    }
+}
