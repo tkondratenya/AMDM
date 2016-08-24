9aaa6f513fa4dc3b5b36a9d1309385f5a053779d
Timofey Kondratenya <t.kondratenya@itransition.com>
2016-08-24 15:25:44 +0300

    Updated Performer.cs

--- AMDM/AMDM.DAL/Entities/Performer.cs
+++ AMDM/AMDM.DAL/Entities/Performer.cs
@@ -11,7 +11,8 @@ namespace AMDM.DAL.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Song> Songs { get; set; }
-        public byte[] ImageData { get; set; }
+        public string ImageLink { get; set; }
+        public string PerformerPageLink { get; set; }
         public string FounderName { get; set; }
         public string BiographyText { get; set; }
     }
